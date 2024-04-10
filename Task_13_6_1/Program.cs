116afb1e05e5304fede604157588e7bab9808235
Денис Голубев <d.golubev@corp.local>
2024-04-10 14:00:37 +0300

    Добавьте файлы проекта.

---
+++ Task_13_6_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_6_1
+{
+    internal class Program
+    {
+
+        public static List<string> list = new List<string>();
+        public static LinkedList<string> linked_list = new LinkedList<string>();
+        public static ReadMyFile file = new ReadMyFile();
+
+        static void Main(string[] args)
+        {
+            var downloads = @"C:\Users\amigo\Downloads\";
+            var path = Path.Combine(downloads, "Text1.txt");
+            var words = file.ReadingFile(path);
+
+            //добавляем в List<string>
+            var stopWatch = Stopwatch.StartNew();
+            foreach ( var word in words )
+            {
+                list.Add(word);
+            }
+            // выводим 
+            Console.WriteLine($"{words.Length} слов вставилось в список за {stopWatch.Elapsed.TotalSeconds} сек.");
+
+            //добавляем в LinkedList<string>
+            var stopWatch_L = Stopwatch.StartNew();
+            foreach (var word in words)
+            {
+                linked_list.AddFirst(word);
+            }
+            // выводим 
+            Console.WriteLine($"{words.Length} слов вставилось в связанный список за {stopWatch.Elapsed.TotalSeconds} сек.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_13_6_1/ReadMyFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_6_1
+{
+    internal class ReadMyFile
+    {
+        public string[] ReadingFile(string path)
+        {
+            // читаем весь файл в строку текста
+            string text = File.ReadAllText(path);
+
+            // Сохраняем символы-разделители в массив
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+            // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            return words;
+        }
+    }
+}
